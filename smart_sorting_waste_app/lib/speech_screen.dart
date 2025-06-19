0566a4d8904c77d560944f41a673b1cdea3d7ffc
JanetAdedosu <adedosuadeola71@gmail.com>
2025-06-19 13:18:45 +0200

    Convert SpeechScreen to StatefulWidget and accept camera list

--- smart_sorting_waste_app/lib/speech_screen.dart
+++ smart_sorting_waste_app/lib/speech_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 class SpeechScreen extends StatelessWidget {
-  const SpeechScreen({Key? key}) : super(key: key);
+  final List<CameraDescription> cameras;
 
+
+  const SpeechScreen({Key? key}) : super(key: key);
+  @override
+  _SpeechScreenState createState() => _SpeechScreenState();
+}
+
+ class _SpeechScreenState extends State<SpeechScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
